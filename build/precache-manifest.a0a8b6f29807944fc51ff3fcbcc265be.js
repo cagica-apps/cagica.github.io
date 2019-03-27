c4b5ffaa8b39beaa6a219672149ede51086727e0
Sérgio Cagica <sergiocagica@hotmail.com>
2019-03-27 23:03:54 +0000

    Build project for Production

---
+++ build/precache-manifest.a0a8b6f29807944fc51ff3fcbcc265be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a36c5f9e0343a0b4868cd737790b447",
+    "url": "/static/media/cv_front.4a36c5f9.png"
+  },
+  {
+    "revision": "044690e6d5c401ef43bdbb374a8b9e87",
+    "url": "/static/media/cv_back.044690e6.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0c08fff593452ba52ec0",
+    "url": "/static/js/main.0d77eb25.chunk.js"
+  },
+  {
+    "revision": "09162ed6cab1871a403e",
+    "url": "/static/js/2.9786579a.chunk.js"
+  },
+  {
+    "revision": "0c08fff593452ba52ec0",
+    "url": "/static/css/main.9ed9a4c1.chunk.css"
+  },
+  {
+    "revision": "42b17d77d0cd029931612532cbb25456",
+    "url": "/index.html"
+  }
+];
